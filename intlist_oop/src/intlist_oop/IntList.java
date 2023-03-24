16fa35fff0c15a9dae5c6c70a5ae497f83658045
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-03-24 12:10:46 +0100

    Implementation using a linked list

--- intlist_oop/src/intlist_oop/IntList.java
+++ intlist_oop/src/intlist_oop/IntList.java
@@ -22,10 +22,41 @@ import java.util.stream.IntStream;
  */
 public class IntList {
 	
+	private class Node {
+		private int value;
+		private Node next;
+		
+		Node(int value, Node next) { this.value = value; this.next = next; }
+	}
+	
+	/**
+	 * @invar | 0 <= size
+	 * @invar | size == 0 ? head == null : getNodes()[size - 1] != null && getNodes()[size - 1].next == null
+	 */
+	private Node head;
+	private int size;
+	
+	/**
+	 * @representationObjects
+	 */
+	private Node[] getNodes() {
+		Node[] result = new Node[size];
+		int i = 0;
+		for (Node n = head; n != null; n = n.next)
+			result[i++] = n;
+		return result;
+	}
+	
 	/**
 	 * @creates | result
 	 */
-	public int[] getElements() { throw new RuntimeException("Not yet implemented"); }
+	public int[] getElements() {
+		int[] result = new int[size];
+		int i = 0;
+		for (Node n = head; n != null; n = n.next)
+			result[i++] = n.value;
+		return result;
+	}
 	
 	/**
 	 * @throws IllegalArgumentException | elements == null
@@ -33,7 +64,16 @@ public class IntList {
 	 * @post The sequence of elements stored in this object equals the given sequence of elements.
 	 *      | Arrays.equals(this.getElements(), elements)
 	 */
-	public IntList(int[] elements) { throw new RuntimeException("Not yet implemented"); }
+	public IntList(int[] elements) {
+		if (elements == null)
+			throw new IllegalArgumentException("`elements` is null");
+		
+		Node n = null;
+		for (int i = elements.length - 1; 0 <= i; i--)
+			n = new Node(elements[i], n);
+		head = n;
+		size = elements.length;
+	}
 
 	/**
 	 * @mutates | this
@@ -42,15 +82,40 @@ public class IntList {
 	 *       |     getElements()[i] == old(getElements())[i])
 	 * @post | getElements()[getElements().length - 1] == element
 	 */
-	public void add(int element) { throw new RuntimeException("Not yet implemented"); }
+	public void add(int element) {
+		if (head == null) {
+			head = new Node(element, null);
+			size = 1;
+		} else {
+			Node n = head;
+			while (n.next != null)
+				n = n.next;
+			n.next = new Node(element, null);
+			size++;
+		}
+	}
 	
 	/**
 	 * @throws IllegalStateException | getElements().length == 0
 	 * @mutates | this
 	 * @post | getElements().length == old(getElements().length) - 1
-	 * @post | Arrays.equals(getElements(), 0, old(getElements().length),
-	 *       |     old(getElements()), 0, old(getElements().length))
+	 * @post | Arrays.equals(getElements(), 0, getElements().length,
+	 *       |     old(getElements()), 0, getElements().length)
 	 */
-	public void removeLast() { throw new RuntimeException("Not yet implemented"); }
+	public void removeLast() {
+		if (head == null)
+			throw new IllegalStateException("The IntList object is empty");
+		
+		if (head.next == null) {
+			head = null;
+			size = 0;
+		} else {
+			Node n = head;
+			while (n.next.next != null)
+				n = n.next;
+			n.next = null;
+			size--;
+		}
+	}
 	
 }
